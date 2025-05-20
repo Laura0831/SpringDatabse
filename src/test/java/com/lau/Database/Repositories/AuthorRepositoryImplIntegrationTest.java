d753fcea868ba0d02c64a4ed3eb6d072173c5e3e
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-05-20 14:26:16 -0700

    adding a file

--- src/test/java/com/lau/Database/Repositories/AuthorRepositoryImplIntegrationTest.java
+++ src/test/java/com/lau/Database/Repositories/AuthorRepositoryImplIntegrationTest.java
@@ -1,47 +1,46 @@
 package com.lau.Database.Repositories;
 
-//import com.lau.Database.TestDataUtil;
-//import com.lau.Database.dao.impl.AuthorDaoImpl;
-//import com.lau.Database.domain.Author;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest
-//@ExtendWith(SpringExtension.class)
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) //this allows for spring boot to remove and
-//// clean the database author creation after every test case is run. This prevents multiple creation of the same author
-//public class AuthorDaoImplIntegrationTest {
-//
-//
-//    private AuthorDaoImpl underTest;
-//
-//
-//    // The autowired annotation tells spring to inject dependencies as declared in this constructor
-//    @Autowired
-//    public AuthorDaoImplIntegrationTest(AuthorDaoImpl underTest){
-//        this.underTest = underTest;
-//    }
-//
-//    @Test
-//    public void testAuthorCreation(){
-//
-//        Author author = TestDataUtil.createTestAuthor();
-//        underTest.create(author);
-//       Optional<Author> result =  underTest.findOne(author.getId());
-//       assertThat(result).isPresent();
-//        assertThat(result.get()).isEqualTo(author);
-//
-//    }
-//
+import com.lau.Database.TestDataUtil;
+import com.lau.Database.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) //this allows for spring boot to remove and
+// clean the database author creation after every test case is run. This prevents multiple creation of the same author
+public class AuthorRepositoryImplIntegrationTest {
+
+
+    private AuthorRepository underTest;
+
+
+    // The autowired annotation tells spring to inject dependencies as declared in this constructor
+    @Autowired
+    public AuthorRepositoryImplIntegrationTest(AuthorRepository underTest){
+        this.underTest = underTest;
+    }
+
+    @Test
+    public void testAuthorCreation(){
+
+        Author author = TestDataUtil.createTestAuthor();
+        underTest.save(author);
+       Optional<Author> result =  underTest.findById(author.getId());
+       assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(author);
+
+    }
+
 //    @Test
 //    public void MultipleAuthorsCreated(){
 //        Author author = TestDataUtil.createTestAuthor();
@@ -56,7 +55,7 @@ package com.lau.Database.Repositories;
 //        assertThat(result).containsExactly(author, author2, author3);
 //
 //    }
-//
+
 //    @Test
 //    public void AuthorUpdated(){
 //        Author author = TestDataUtil.createTestAuthor();
@@ -69,7 +68,7 @@ package com.lau.Database.Repositories;
 //
 //
 //    }
-//
+
 //
 //    @Test
 //    public void AuthorDelete(){
@@ -79,6 +78,6 @@ package com.lau.Database.Repositories;
 //        Optional<Author> result = underTest.findOne(author.getId());
 //        assertThat(result).isEmpty();
 //    }
-//
-//
-//}
+
+
+}
