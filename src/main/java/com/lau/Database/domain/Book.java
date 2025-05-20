2f1189579a6da6a58be42aaf22248409967e81b3
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-05-19 17:24:03 -0700

    Modify Author and Book class to follow the syntax of Hibernate

--- src/main/java/com/lau/Database/domain/Book.java
+++ src/main/java/com/lau/Database/domain/Book.java
@@ -1,6 +1,7 @@
 package com.lau.Database.domain;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,11 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
     private String isbn;
+
     private String title;
-    private Long authorId;
+
+    @ManyToOne(cascade = CascadeType.ALL) //this means if we get a book back, we also get to retrieve the author and all the values in the author class
+    @JoinColumn(name = "author_id")
+    private Author author;
 
 
 }
