55cda022b9ffbc4bdcb2012e7055d4b575d254ba
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-06-18 16:01:07 -0700

    Created a second integration test to check the return values of the created author object

--- src/test/java/com/lau/Database/controllers/AuthorControllerIntegrationTest.java
+++ src/test/java/com/lau/Database/controllers/AuthorControllerIntegrationTest.java
@@ -30,7 +30,7 @@ public class AuthorControllerIntegrationTest {
 
 
 
-    @Test
+    @Test //test to make sure author was created successfully
     public void CreateAuthorSuccessfully() throws Exception {
         AuthorEntity author1 = TestDataUtil.createTestAuthor(); //creates author
         author1.setId(null);
@@ -42,6 +42,23 @@ public class AuthorControllerIntegrationTest {
                         .content(authorJSON) //We need to post a JSON object to the endpoint
         )
                 .andExpect(MockMvcResultMatchers.status().isCreated()); //similar to expectTrue on Junit
+    }
+
+
+    @Test //Test to make sure author was created successfully by return set author
+    public void CreateAuthorSuccessfullyAndReturnAuthor() throws Exception {
+        AuthorEntity author1 = TestDataUtil.createTestAuthor2(); //creates author
+        author1.setId(null);
+        String authorJSON = objMap.writeValueAsString(author1);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/authors")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(authorJSON) //We need to post a JSON object to the endpoint
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Emily Henry"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(45)); //similar to AssertTrue on Junit
     }
 
 }
